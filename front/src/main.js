c433058e7cbfc25ff0a4535600ad80d4641a03d9
Giranda22-Git <61464174+Giranda22-Git@users.noreply.github.com>
2021-03-25 19:08:39 +0600

    Revert "let index = 0"
    This reverts commit bc84664f1e6bf358df3844d2d149601f864c3941.

--- front/src/main.js
+++ front/src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vueChatScroll from 'vue-chat-scroll'
-import './tailwind.css'
 
 Vue.config.productionTip = false
 Vue.use(vueChatScroll)
